35fe64d7d3cb9fca4892b5489332566eae791586
İsmail HASIR <ismailhasir@gmail.com>
2022-02-26 19:29:47 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button>New Game</button>
+    </div>
+  );
+}
+
+export default App
